a6b3ec06c9a6cfa309ba4da2765e133a70e0c34a
Martin <mfmgamboa@gmail.com>
2025-07-11 10:22:37 +0800

    Initial commit: Laravel + React setup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home - Virtual Job Fair</title>
+</head>
+<body>
+    <h1>Welcome to the Job Fair Platform!</h1>
+    <p>This is your custom home page.</p>
+</body>
+</html>
